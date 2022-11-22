ccb0d045486c3736f3aead0339fe91cc9bd6e0d5
user <putin900307@gmail.com>
2022-11-22 22:14:54 +0200

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def about(request):
+    a= 5+6
+    return render(request, 'about.html', {'gretting': a})
+
+def home(request):
+    return HttpResponse('this is my home')
